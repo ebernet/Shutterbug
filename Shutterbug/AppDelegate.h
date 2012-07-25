5ca5b1cdcded40bf6db24c7e67fcf32b751de6cd
Eytan Bernet <ebernet@mac.com>
2012-07-25 15:18:30 -0700

    Initial Commit

---
+++ Shutterbug/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Shutterbug
+//
+//  Created by Eytan Bernet on 7/25/12.
+//  Copyright (c) 2012 Eytan Bernet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Shutterbug/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Shutterbug
+//
+//  Created by Eytan Bernet on 7/25/12.
+//  Copyright (c) 2012 Eytan Bernet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
